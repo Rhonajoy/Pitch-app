51b3b8d0fa4057a6a5ba7e3cdf3d76ac6152c2ac
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-06 22:52:41 +0300

    creates tests for user model

--- app/main/views.py
+++ app/main/views.py
@@ -8,27 +8,15 @@ from .forms import  CategoryForm
 @main.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+    # pitches = Post.query.order_by(Post.timestamp.desc()).all()
 
-    # Getting popular movie
-    popular_movies = get_movies('popular')
-    upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
+    return render_template('index.html')
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    
+        
 
-    search_movie = request.args.get('movie_query')
-
-    if search_movie:
-        return redirect(url_for('.search',movie_name=search_movie))
-    else:
-        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-
-
-@main.route('/add_category', methods=['GET', 'POST'])
-@login_required
+@main.route('/category', methods=['GET', 'POST'])
+# @login_required
 def add_category():
     form = CategoryForm()
     if form.validate_on_submit():
@@ -37,7 +25,7 @@ def add_category():
         db.session.commit()
         flash('Category added successfully.')
         return redirect(url_for('.index'))
-    return render_template('add_category.html', form=form)
+    return render_template('category.html', form=form)
 
 
 
