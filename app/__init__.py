5f2155cd7577ca0894097441165bc714bf87bde2
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-06 12:59:01 +0300

    creates a main Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -19,26 +19,6 @@ photos = UploadSet('photos',IMAGES)
 mail = Mail()
 simple = SimpleMDE()
 
-from flask import Flask
-from config import config_options
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_mail import Mail
-from flask_simplemde import SimpleMDE
-
-
-db = SQLAlchemy()
-bootstrap = Bootstrap()
-mail = Mail()
-simple = SimpleMDE()
-login_manager = LoginManager()
-
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-photos = UploadSet('photos', IMAGES)
-
 
 # creating the application
 
