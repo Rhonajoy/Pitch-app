b0e83b874b8f04f940a1e97aab6361a91d82db8d
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-06 23:32:46 +0300

    adds the registration template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,15 +5,17 @@ from ..models import User
 from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
-    username = StringField('Enter your username',validators = [Required()])
-    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    submit = SubmitField('Sign Up')
-    def validate_email(self,data_field):
+   first_name = StringField('Your First Nmae',validators=[Required()])
+   last_name = StringField('Your Last Name',validators=[Required()])
+   email = StringField('Your Email Address',validators=[Required(),Email()])
+   username = StringField('Enter your username',validators = [Required()])
+   password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+   password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+   submit = SubmitField('Sign Up')
+   def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
 
-    def validate_username(self,data_field):
+   def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
--- app/auth/views.py
+++ app/auth/views.py
@@ -12,7 +12,7 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(first_name = form.first_name.data,last_name = form.last_name.data,email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
