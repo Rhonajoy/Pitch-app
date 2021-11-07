09da79acdb95b857a50ee6c757c621e333e7bed5
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-07 20:32:55 +0300

    adds the email that users receive on sign up

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
 from . import auth
 from flask import render_template,redirect,url_for, flash,request
 from ..models import User
+from ..email import mail_message
 from .forms import LoginForm,RegistrationForm
 from flask_login import login_user,logout_user,login_required
 from .. import db
@@ -12,8 +13,10 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('auth.login'))
+        
         title = "New Account"
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html',registration_form = form)
 # login
 @auth.route('/login',methods=['GET','POST'])
@@ -34,3 +37,4 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template,request, redirect, url_for, abort, flash
 from . import main
 from flask_login import login_required, current_user
 from ..models import User,  Category,Pitches
@@ -81,6 +81,17 @@ def new_pitch():
     return redirect(url_for('main.index'))
 
   return render_template('pitches.html', form=form)
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
 
 
 
