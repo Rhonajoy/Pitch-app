9eaed556ec21036f8be7133914e2b3d62376fed7
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-10 00:37:53 +0300

    adds footer color

--- app/main/views.py
+++ app/main/views.py
@@ -25,19 +25,12 @@ def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
-
     if form.validate_on_submit():
         user.bio = form.bio.data
-
         db.session.add(user)
         db.session.commit()
-
         return redirect(url_for('.profile',uname=user.username))
-
-
     return render_template('profile/update.html',form =form)
-
-
 @main.route('/createpitch', methods=['GET', 'POST'])
 @login_required
 def new_pitch():
@@ -50,10 +43,6 @@ def new_pitch():
         new_pitch.save_pitches()
         
         return redirect(url_for('main.index'))
-
-   
-        
-
     return render_template('pitch.html', pitch_form = pitch_form)
 
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
@@ -69,48 +58,41 @@ def update_pic(uname):
 @main.route('/comments/<pitch_id>', methods=['GET', 'POST'])
 @login_required
 def comments(pitch_id):
-    
-    # get all comments of the pitch
     comments = Comment.query.filter_by(pitch_id=pitch_id).all()
     pitch = Pitches.query.get(pitch_id)
     form = CommentForm()
     if pitch is None:
         abort(404)
-    
     if form.validate_on_submit():
             comment = Comment(
             content=form.content.data,
             pitch_id=pitch_id,
             user_id=current_user.id
-
         )
             db.session.add(comment)
             db.session.commit()
             form.content.data = ''
             flash('Your comment has been posted successfully!')
     return render_template('comments.html',pitches= pitch, comment=comments, form = form)
+
 @main.route('/like/<pitch_id>', methods=['GET', 'POST'])
 @login_required
 def like(pitch_id):
     pitch = Pitches.query.get(pitch_id)
     if pitch is None:
         abort(404)
-    # check if the user has already liked the pitch
     like = Likes.query.filter_by(user_id=current_user.id, pitch_id=pitch_id).first()
     if like is not None:
-        # if the user has already liked the pitch, delete the like
         db.session.delete(like)
         db.session.commit()
         flash('You have successfully unlike the pitch!')
         return redirect(url_for('.index'))
-    # if the user has not liked the pitch, add a like
     new_like = Likes(
         user_id=current_user.id,
         pitch_id=pitch_id
     )
     db.session.add(new_like)
     db.session.commit()
-    
     return redirect(url_for('.index'))
 @main.route('/dislike/<pitch_id>', methods=['GET', 'POST'])
 @login_required
@@ -118,22 +100,17 @@ def dislike(pitch_id):
     pitch = Pitches.query.get(pitch_id)
     if pitch is None:
         abort(404)
-    # check if the user has already liked the pitch
     dislike =  Dislikes.query.filter_by(user_id=current_user.id, pitch_id=pitch_id).first()
     if dislike is not None:
-        # if the user has already liked the pitch, delete the like
         db.session.delete(dislike)
         db.session.commit()
-        
         return redirect(url_for('.index'))
-    # if the user has not liked the pitch, add a like
     new_dislike = Dislikes(
         user_id=current_user.id,
         pitch_id=pitch_id
     )
     db.session.add(new_dislike)
     db.session.commit()
-    
     return redirect(url_for('.index'))
 
 
