860726e90b24b783577c8c6d0f7900e764283c32
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-10 11:43:16 +0300

    deployment

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 migrate = Migrate(app,db)
 manager = Manager(app)
